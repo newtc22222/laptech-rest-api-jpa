c1cff180485354ae5540e736137efceafdae9a85
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-02-05 16:19:40 +0700

    Initial commit

---
+++ src/main/java/com/laptech/restapi/LaptechRestApiJpaApplication.java
@@ -0,0 +1,13 @@
+package com.laptech.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptechRestApiJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptechRestApiJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laptech/restapi/LaptechRestApiJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laptech.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptechRestApiJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
